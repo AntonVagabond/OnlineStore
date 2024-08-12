0a3f6624b6acc18e08b96ff14dd670c4683f443e
Akuev <antonakuev945@mail.ru>
2024-08-12 14:38:11 +0300

    fix [User]: Перенес получение текущего пользователя в файл dependencies.

--- src/apps/user/api/dependencies.py
+++ src/apps/user/api/dependencies.py
@@ -1,15 +1,53 @@
-from typing import Annotated
+from typing import Annotated, Optional, Callable, Coroutine, Any
+from uuid import UUID
 
-from fastapi import Depends
+import httpx
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 
-from common.interfaces.abstraction_uow import IUnitOfWork
 from common.schemas.api.mixins import CurrentUserSchema
-from common.services.base import BaseService
-from common.unit_of_works.base import BaseUnitOfWork
-from core.security import get_current_user
-from modules.repositories.profiles import ProfileRepository
+from core.config import get_settings
 from modules.services.profiles import ProfileService
 from modules.unit_of_works.profiles import ProfileUOW
+
+settings = get_settings()
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=str(settings.token_url), scheme_name="JWT",
+)
+
+
+# region ------------------------------------- USER --------------------------------------
+def get_current_user(
+        roles: Optional[tuple[str, ...]] = None,
+) -> Callable[[str], Coroutine[Any, Any, CurrentUserSchema]]:
+    """Возвращает авторизованного пользователя."""
+
+    async def current_user(token: str = Depends(oauth2_scheme)) -> CurrentUserSchema:
+        """Поиск текущего пользователя."""
+        async with httpx.AsyncClient() as client:
+            data = {"access_token": token, "roles": roles}
+            filter_data = dict(filter(lambda x: x[1] is not None, data.items()))
+            if filter_data.get("roles") is not None:
+                filter_data["roles"] = ', '.join(filter_data["roles"])  # noqa
+
+            response = await client.get(
+                url=str(settings.authenticate_url), headers=filter_data,
+            )
+
+            user_data_or_exc = response.json()
+            if bool(user_data_or_exc.get("status_code")):
+                raise HTTPException(
+                    status_code=user_data_or_exc["status_code"],
+                    detail=user_data_or_exc["detail"],
+                    headers=user_data_or_exc["headers"],
+                )
+            return CurrentUserSchema(id=UUID(user_data_or_exc["id"]))
+
+    return current_user
+
+
+# endregion ------------------------------------------------------------------------------
+
 
 # region ---------------------------------- COMMON ---------------------------------------
 UserDep = Annotated[CurrentUserSchema, Depends(get_current_user())]
