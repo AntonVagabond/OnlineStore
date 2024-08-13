7451651015dd1f65d9e291bf49618a9c1b204ade
Akuev <antonakuev945@mail.ru>
2024-08-13 14:52:01 +0300

    feat [Users]: Добавил валидацию для параметра birthday.

--- src/apps/user/modules/schemas/profile.py
+++ src/apps/user/modules/schemas/profile.py
@@ -1,20 +1,26 @@
 from datetime import datetime
 from typing import Optional
+from uuid import UUID
 
-from pydantic import EmailStr
+from pydantic import EmailStr, field_validator
 
 from common.schemas.base import BaseModel
 
 
 class ProfileResponseSchema(BaseModel):
     """Схема ответа на запрос профиля."""
-    id: str
+    id: UUID
     last_name: str
     first_name: str
     second_name: str
     phone_number: str
     email: EmailStr
     is_man: bool
-    photo: Optional[...]
+    photo: Optional[str]
     birthday: datetime
-    date_added: str
+
+    @field_validator("birthday")
+    def validate_birthday(cls, birthday: datetime) -> datetime:  # noqa
+        """Преобразование даты рождения под нужный формат."""
+        datetime_str = birthday.strftime("%Y-%m-%d %H:%M:%S")
+        return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
