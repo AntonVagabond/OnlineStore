e96a44cb559d6ec41211d9727f9b9a36995b22b2
Akuev <antonakuev945@mail.ru>
2024-08-14 13:40:34 +0300

    fix [User]: Изменил методы экземпляра на классовые методы.

--- src/apps/user/common/interfaces/abstraction_service.py
+++ src/apps/user/common/interfaces/abstraction_service.py
@@ -3,13 +3,12 @@ from typing import Any, Optional, TypeVar
 from uuid import UUID
 
 from common.models.base import Base
-from common.schemas.base import BaseModel
 from common.schemas.filters.mixins import BaseFilterSchema
 from common.schemas.internal.mixins import PageViewSchema
 from common.unit_of_works.base import BaseUnitOfWork
 
 TModel = TypeVar("TModel", bound=Base)
-TSchema = TypeVar("TSchema", bound=BaseModel)
+TDict = TypeVar("TDict", bound=dict)
 TFilter = TypeVar("TFilter", bound=BaseFilterSchema)
 TUnitOfWork = TypeVar("TUnitOfWork", bound=BaseUnitOfWork)
 TID = TypeVar("TID", int, UUID)
@@ -18,32 +17,39 @@ TID = TypeVar("TID", int, UUID)
 class IService(abc.ABC):
     """Абстрактный класс сервиса."""
 
+    @classmethod
     @abc.abstractmethod
-    async def add(self, uow: TUnitOfWork, obj: TSchema) -> Any:
+    async def add(cls, uow: TUnitOfWork, obj_dict: TDict) -> Any:
         """Абстрактный метод сервиса для добавления данных."""
 
+    @classmethod
     @abc.abstractmethod
-    async def get(self, uow: TUnitOfWork, obj_id: TID) -> Optional[TModel]:
+    async def get(cls, uow: TUnitOfWork, obj_id: TID) -> Optional[TModel]:
         """Абстрактный метод сервиса для получения данных."""
 
+    @classmethod
     @abc.abstractmethod
     async def get_all(
-            self, uow: TUnitOfWork, filters: Optional[TFilter],
+            cls, uow: TUnitOfWork, filters: Optional[TFilter],
     ) -> PageViewSchema:
         """Абстрактный метод сервиса для получения списка данных."""
 
+    @classmethod
     @abc.abstractmethod
-    async def delete(self, uow: TUnitOfWork, obj_id: TID) -> Optional[TID]:
+    async def delete(cls, uow: TUnitOfWork, obj_id: TID) -> Optional[TID]:
         """Абстрактный метод сервиса для обновления статуса данных на "удалено"."""
 
+    @classmethod
     @abc.abstractmethod
-    async def delete_db(self, uow: TUnitOfWork, obj_id: TID) -> bool:
+    async def delete_db(cls, uow: TUnitOfWork, obj_id: TID) -> bool:
         """Абстрактный метод сервиса для удаления данных из базы данных."""
 
+    @classmethod
     @abc.abstractmethod
-    async def edit(self, uow: TUnitOfWork, obj: TSchema) -> bool:
+    async def edit(cls, uow: TUnitOfWork, obj_dict: TDict) -> bool:
         """Абстрактный метод сервиса для редактирования данных."""
 
+    @classmethod
     @abc.abstractmethod
-    async def exist(self, uow: TUnitOfWork, obj_id: TID) -> bool:
+    async def exist(cls, uow: TUnitOfWork, obj_id: TID) -> bool:
         """Абстрактный метод сервиса для поиска данных."""
--- src/apps/user/common/services/base.py
+++ src/apps/user/common/services/base.py
@@ -3,13 +3,12 @@ from uuid import UUID
 
 from common.interfaces.abstraction_service import IService
 from common.models.base import Base
-from common.schemas.base import BaseModel
 from common.schemas.filters.mixins import BaseFilterSchema
 from common.schemas.internal.mixins import PageViewSchema
 from common.unit_of_works.base import BaseUnitOfWork
 
 TModel = TypeVar("TModel", bound=Base)
-TSchema = TypeVar("TSchema", bound=BaseModel)
+TDict = TypeVar("TDict", bound=dict)
 TFilter = TypeVar("TFilter", bound=BaseFilterSchema)
 TUnitOfWork = TypeVar("TUnitOfWork", bound=BaseUnitOfWork)
 TID = TypeVar("TID", int, UUID)
@@ -18,48 +17,53 @@ TID = TypeVar("TID", int, UUID)
 class BaseService(IService):
     """Базовый класс сервиса."""
 
-    async def add(self, uow: TUnitOfWork, obj: TSchema) -> TID:
+    @classmethod
+    async def add(cls, uow: TUnitOfWork, obj_dict: TDict) -> TID:
         """Базовый метод сервиса для добавления данных."""
-        obj_dict = obj.model_dump()
         async with uow:
             obj_id = await uow.repo.add(obj_dict)
             await uow.commit()
             return obj_id
 
-    async def get(self, uow: TUnitOfWork, obj_id: TID) -> Optional[TModel]:
+    @classmethod
+    async def get(cls, uow: TUnitOfWork, obj_id: TID) -> Optional[TModel]:
         """Базовый метод сервиса для получения данных."""
         async with uow:
             obj = await uow.repo.get(obj_id)
             return obj
 
+    @classmethod
     async def get_all(
-            self, uow: TUnitOfWork, filters: Optional[TFilter],
+            cls, uow: TUnitOfWork, filters: Optional[TFilter],
     ) -> PageViewSchema:
         """Базовый метод сервиса для получения списка данных."""
 
-    async def delete(self, uow: TUnitOfWork, obj_id: TID) -> Optional[TID]:
+    @classmethod
+    async def delete(cls, uow: TUnitOfWork, obj_id: TID) -> Optional[TID]:
         """Базовый метод сервиса для обновления статуса данных на "удалено"."""
         async with uow:
             result = await uow.repo.delete(obj_id)
             await uow.commit()
             return result
 
-    async def delete_db(self, uow: TUnitOfWork, obj_id: TID) -> bool:
+    @classmethod
+    async def delete_db(cls, uow: TUnitOfWork, obj_id: TID) -> bool:
         """Базовый метод сервиса для удаления данных из базы данных."""
         async with uow:
             result = await uow.repo.delete_db(obj_id)
             await uow.commit()
             return result
 
-    async def edit(self, uow: TUnitOfWork, obj: TSchema) -> bool:
+    @classmethod
+    async def edit(cls, uow: TUnitOfWork, obj_dict: TDict) -> bool:
         """Базовый метод сервиса для редактирования данных."""
-        obj_dict = obj.model_dump()
         async with uow:
             result = await uow.repo.edit(obj_dict)
             await uow.commit()
             return result
 
-    async def exist(self, uow: TUnitOfWork, obj_id: TID) -> bool:
+    @classmethod
+    async def exist(cls, uow: TUnitOfWork, obj_id: TID) -> bool:
         """Базовый метод сервиса для поиска данных."""
         async with uow:
             result = await uow.repo.exist(obj_id)
