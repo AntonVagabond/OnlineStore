b6785bf3709031d592f9a49dc6684b0d5e206957
Akuev <antonakuev945@mail.ru>
2024-08-14 15:34:51 +0300

    fix: Переименовал микросервис с user -> users.

--- src/apps/users/api/v1/controllers/profiles.py
+++ src/apps/users/api/v1/controllers/profiles.py
@@ -1,14 +1,46 @@
 import logging
-from typing import Union
 
 from fastapi import APIRouter
 from starlette.responses import JSONResponse, Response
 
 from api.dependencies import UserDep, ProfileUOWDep, ProfileServiceDep
+from common.schemas.api.mixins import RegisterSchema
 from common.schemas.responses import mixins as response
-from modules.schemas.profile import ProfileResponseSchema
+from modules.schemas.profiles import ProfileResponseSchema
 
 profile = APIRouter(prefix="/api/v1/profile", tags=["Profile"])
+
+
+@profile.post(
+    path="/register/",
+    summary="Регистрация пользователя.",
+    responses={
+        # 200: {"model": response.SuccessResponseSchema},
+        400: {"model": response.BadRequestResponseSchema},
+        401: {"model": response.UnauthorizedResponseSchema},
+        403: {"model": response.ForbiddenResponseSchema},
+        500: {"model": response.ServerErrorResponseSchema},
+    }
+)
+async def create_user(
+        current_user: UserDep,  # noqa
+        uow: ProfileUOWDep,
+        service: ProfileServiceDep,
+        model: RegisterSchema,
+) -> Response:
+    """Контроллер регистрации пользователя."""
+    result = await service.add(uow, model)
+    if isinstance(result, str):
+        return JSONResponse(status_code=400, content={"message": result})
+    logging.info(
+        {
+            "method": "create_user",
+            "endpoint": "/register/",
+            "status_code": 200,
+            "message": "Пользователь успешно создан",
+        }
+    )
+    return JSONResponse(status_code=200, content={"message": "Пользователь успешно создан"})
 
 
 @profile.get(
@@ -22,7 +54,7 @@ profile = APIRouter(prefix="/api/v1/profile", tags=["Profile"])
         500: {"model": response.ServerErrorResponseSchema},
     },
 )
-async def get_user_info(
+async def get_user(
         current_user: UserDep, uow: ProfileUOWDep, service: ProfileServiceDep,
 ) -> Response:
     """Контроллер получения информации профиля пользователя."""
--- src/apps/users/modules/services/profiles.py
+++ src/apps/users/modules/services/profiles.py
@@ -1,14 +1,14 @@
-import base64
 from datetime import datetime
 from types import NoneType
 from typing import Optional, Union, TypeAlias
 from uuid import UUID
 
 from common.enums.role import Role
+from common.schemas.api.mixins import RegisterSchema
 from common.services.base import BaseService
+from core.security import hash_password
 from models.users import User
-from modules.repositories.profiles import ProfileRepository
-from modules.schemas.profile import ProfileResponseSchema
+from modules.schemas.profiles import ProfileResponseSchema
 from modules.unit_of_works.profiles import ProfileUOW
 
 RegisterData: TypeAlias = dict[
@@ -20,6 +20,29 @@ EditData: TypeAlias = dict[str, Union[UUID, str, bool, datetime, int, None]]
 class ProfileService(BaseService):
     """Сервис для работы с профилем."""
 
+    # region --------------------------------- CREATE ------------------------------------
+    @staticmethod
+    def __add_user_data(user_data: RegisterData) -> RegisterData:
+        """Добавить данные пользователя в БД."""
+        user_data.update(username=user_data["email"])
+        user_data.update(password_hash=hash_password(user_data["password_hash"]))
+        last_name, first_name, second_name = (
+            user_data['last_name'], user_data['first_name'], user_data['second_name'],
+        )
+        user_data.update(full_name=f"{last_name} {first_name} {second_name}")
+        return user_data
+
+    @classmethod
+    async def create(cls, uow: ProfileUOW, schema: RegisterSchema) -> str:
+        """Создать нового пользователя."""
+        user_data = schema.model_dump()
+        user_update_data = cls.__add_user_data(user_data)
+        result = await cls.add(uow=uow, obj_dict=user_update_data)
+        return result
+
+    # endregion --------------------------------------------------------------------------
+
+    # region ------------------------------- GET -----------------------------------------
     @staticmethod
     def __convert_result(result: User) -> ProfileResponseSchema:
         """Конвертировать результат в модель."""
@@ -41,3 +64,19 @@ class ProfileService(BaseService):
         async with uow:
             current_user = await uow.repo.get(user_id)
             return cls.__convert_result(current_user) if current_user else None
+
+    # endregion --------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
