1e4e2b4dc3ee406ad6fa69ea408d099443ca40a4
Akuev <antonakuev945@mail.ru>
2024-08-14 09:19:44 +0300

    fix [User]: Убрал ненужные дженерики.

--- src/apps/user/common/schemas/responses/mixins.py
+++ src/apps/user/common/schemas/responses/mixins.py
@@ -1,27 +1,23 @@
-from typing import Generic, TypeVar
-
 from pydantic import Field
 
 from common.schemas.base import BaseModel
 
-TSchema = TypeVar("TSchema", bound=BaseModel)
 
-
-class UnauthorizedResponseSchema(BaseModel, Generic[TSchema]):
+class UnauthorizedResponseSchema(BaseModel):
     """Схема ответа при неавторизованном запросе."""
     detail: str = Field(default="Не авторизованный пользователь.")
 
 
-class ForbiddenResponseSchema(BaseModel, Generic[TSchema]):
+class ForbiddenResponseSchema(BaseModel):
     """Схема ответа при неавторизованном запросе."""
     detail: str = Field(default="Нет прав доступа.")
 
 
-class NotFoundResponseSchema(BaseModel, Generic[TSchema]):
+class NotFoundResponseSchema(BaseModel):
     """Схема ответа при не найденном ресурсе."""
     detail: str = Field(default="Данные не найдены.")
 
 
-class ServerErrorResponseSchema(BaseModel, Generic[TSchema]):
+class ServerErrorResponseSchema(BaseModel):
     """Схема ответа при неавторизованном запросе."""
     detail: str = Field(default="Ошибка сервера")
