a9a57e332fafc54a023845ad9842ae83dd58f478
Akuev <antonakuev945@mail.ru>
2024-08-14 09:20:39 +0300

    feat [Auth]: Добавил модели ответов.

---
+++ src/apps/auth/common/schemas/responses/mixins.py
@@ -0,0 +1,28 @@
+from pydantic import Field
+
+from ..base import BaseModel
+
+
+class BadRequestResponseSchema(BaseModel):
+    """Схема ответа при неправильном запросе."""
+    detail: str = Field(default="Некорректные данные.")
+
+
+class UnauthorizedResponseSchema(BaseModel):
+    """Схема ответа при неавторизованном запросе."""
+    detail: str = Field(default="Не авторизованный пользователь.")
+
+
+class ForbiddenResponseSchema(BaseModel):
+    """Схема ответа при неавторизованном запросе."""
+    detail: str = Field(default="Нет прав доступа.")
+
+
+class NotFoundResponseSchema(BaseModel):
+    """Схема ответа при не найденном ресурсе."""
+    detail: str = Field(default="Данные не найдены.")
+
+
+class ServerErrorResponseSchema(BaseModel):
+    """Схема ответа при неавторизованном запросе."""
+    detail: str = Field(default="Ошибка сервера")
