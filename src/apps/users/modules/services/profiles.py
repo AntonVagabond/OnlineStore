7b964248396db828238df3783cc7192c6cae6e1d
Akuev <antonakuev945@mail.ru>
2024-08-15 15:16:14 +0300

    feat [User]: Добавил бизнес логику для обновления профиля.

--- src/apps/users/modules/services/profiles.py
+++ src/apps/users/modules/services/profiles.py
@@ -9,7 +9,7 @@ from common.schemas.api.mixins import RegisterSchema
 from common.services.base import BaseService
 from core.security import hash_password
 from models.users import User
-from modules.schemas.profiles import ProfileResponseSchema
+from modules.schemas.profiles import ProfileResponseSchema, UpdateUserSchema
 from modules.unit_of_works.profiles import ProfileUOW
 
 RegisterData: TypeAlias = dict[
@@ -23,8 +23,8 @@ class ProfileService(BaseService):
 
     # region --------------------------------- CREATE ------------------------------------
     @staticmethod
-    def __add_user_data(user_data: RegisterData) -> RegisterData:
-        """Добавить данные пользователя в БД."""
+    def __add_data_for_create(user_data: RegisterData) -> RegisterData:
+        """Добавить данные для создания пользователя."""
         user_data.update(username=user_data["email"])
         user_data.update(password_hash=hash_password(user_data["password_hash"]))
         last_name, first_name, second_name = (
@@ -37,7 +37,7 @@ class ProfileService(BaseService):
     async def create(cls, uow: ProfileUOW, schema: RegisterSchema) -> str:
         """Создать нового пользователя."""
         user_data = schema.model_dump()
-        user_update_data = cls.__add_user_data(user_data)
+        user_update_data = cls.__add_data_for_create(user_data)
         result = await cls.add(uow=uow, obj_dict=user_update_data)
         return result
 
@@ -68,4 +68,26 @@ class ProfileService(BaseService):
                 raise error.UserNotFoundException()
             return cls.__convert_result(current_user)
 
+    # endregion --------------------------------------------------------------------------
+
+    # region --------------------------------- EDIT --------------------------------------
+    @staticmethod
+    def __add_data_for_update(user_data: EditData, user_id: UUID) -> EditData:
+        """Добавить данные для редактирования пользователя."""
+        user_data.update(id=user_id)
+        last_name, first_name, second_name = (
+            user_data['last_name'], user_data['first_name'], user_data['second_name'],
+        )
+        user_data.update(full_name=f"{last_name} {first_name} {second_name}")
+        return user_data
+
+    @classmethod
+    async def update(
+            cls, uow: ProfileUOW, model: UpdateUserSchema, user_id: UUID,
+    ) -> bool:
+        """Обновить данные пользователя."""
+        user_data = model.model_dump()
+        user_update_data = cls.__add_data_for_update(user_data, user_id)
+        result = await cls.edit(uow=uow, obj_dict=user_update_data)
+        return result
     # endregion --------------------------------------------------------------------------
