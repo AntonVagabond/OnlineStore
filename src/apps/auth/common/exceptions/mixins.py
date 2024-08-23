e5ebbef1c90e635ea46fe51cb4f1f4ca5648a805
Akuev <antonakuev945@mail.ru>
2024-08-23 08:45:53 +0300

    feat [Auth]: Добавил кастомные исключения.

---
+++ src/apps/auth/common/exceptions/mixins.py
@@ -0,0 +1,37 @@
+from fastapi import HTTPException
+
+
+class AuthUnauthorizedException(HTTPException):
+    """Исключение, при неправильном или истекшем токене аутентификации."""
+
+    def __init__(self) -> None:
+        self.status_code = 401
+        self.detail = "Не удалось подтвердить учетные данные",
+        self.headers = {"WWW-Authenticate": "Bearer"}
+        super().__init__(
+            status_code=self.status_code, detail=self.detail, headers=self.headers,
+        )
+
+
+class AuthForbiddenException(HTTPException):
+    """Исключение, при запрещенном доступе."""
+
+    def __init__(self, detail: str) -> None:
+        self.status_code = 403
+        self.detail = detail
+        self.headers = {"WWW-Authenticate": "Bearer"}
+        super().__init__(
+            status_code=self.status_code, detail=self.detail, headers=self.headers,
+        )
+
+
+class AuthBadRequestException(HTTPException):
+    """Исключение, при неправильном запросе."""
+
+    def __init__(self, detail: str) -> None:
+        self.status_code = 400
+        self.detail = detail
+        self.headers = {"WWW-Authenticate": "Bearer"}
+        super().__init__(
+            status_code=self.status_code, detail=self.detail, headers=self.headers,
+        )
