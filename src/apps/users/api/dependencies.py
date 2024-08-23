e8d2c764b664663b140e35a9ed811ec8eeb9ec5c
Akuev <antonakuev945@mail.ru>
2024-08-23 08:47:38 +0300

    fix [User]: Переименовал файл и перенес в директорию clients.

--- src/apps/users/api/dependencies.py
+++ src/apps/users/api/dependencies.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from api.current_user_deps import get_current_user
+from clients.auth import get_current_user
 from common.schemas.api.mixins import CurrentUserSchema
 from modules.services.profiles import ProfileService
 from modules.unit_of_works.profiles import ProfileUOW
