30476cb8628d451b63e5ee2140e46ff2caf7af09
Akuev <antonakuev945@mail.ru>
2024-08-26 13:11:28 +0300

    fix [Auth]: Незначительные изменения.

--- src/apps/auth/api/current_user_deps.py
+++ src/apps/auth/api/current_user_deps.py
@@ -55,7 +55,7 @@ class CurrentUserDep:
             if not is_valid_role:
                 raise error.AuthForbiddenException(
                     detail=f"Для этого действия требуется одна из "
-                           f"ролей: '{', '.join(req_role for req_role in roles)}'",
+                           f"ролей: '{', '.join(req_role for req_role in roles)}'.",
                 )
         return data_user
 
