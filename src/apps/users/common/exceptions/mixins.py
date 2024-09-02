ddccdd4043ab65fd651faa6485ff898a433ad0c7
Akuev <antonakuev945@mail.ru>
2024-09-02 16:11:24 +0300

    fix [Users]: Незначительные изменения.

--- src/apps/users/common/exceptions/mixins.py
+++ src/apps/users/common/exceptions/mixins.py
@@ -1,11 +1,12 @@
 from fastapi import HTTPException
+from common.const import response_exception as resp_exc
 
 
 class UserNotFoundException(HTTPException):
     """Исключение, при отсутствии пользователя в базе данных."""
     def __init__(self) -> None:
         self.status_code = 404
-        self.detail = "Пользователь не найден."
+        self.detail = resp_exc.USER_NOT_FOUND
         super().__init__(status_code=self.status_code, detail=self.detail)
 
 
@@ -13,7 +14,7 @@ class EmailAlreadyExistsException(HTTPException):
     """Исключение, при уже существующей электронной почте в базе данных."""
     def __init__(self) -> None:
         self.status_code = 409
-        self.detail = "Такая почта уже существует."
+        self.detail = resp_exc.EMAIL_CONFLICT
         super().__init__(status_code=self.status_code, detail=self.detail)
 
 
@@ -21,5 +22,5 @@ class PhoneNumberAlreadyExistsException(HTTPException):
     """Исключение, при уже существующем номере телефона в базе данных."""
     def __init__(self) -> None:
         self.status_code = 409
-        self.detail = "Такой номер телефона уже существует."
+        self.detail = resp_exc.PHONE_NUMBER_CONFLICT
         super().__init__(status_code=self.status_code, detail=self.detail)
