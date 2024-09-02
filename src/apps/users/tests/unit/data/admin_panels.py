2cd4b26a34568469877df0bdd767f719c2a7a973
Akuev <antonakuev945@mail.ru>
2024-09-02 16:02:21 +0300

    fix [Users]: Добавил тестовые данные для админ панели.

---
+++ src/apps/users/tests/unit/data/admin_panels.py
@@ -0,0 +1,104 @@
+GET_LIST_USERS_ADMIN_TEST_DATA = (
+    "test_data",
+    (
+        {
+            "page_number": 0,
+            "page_size": 10,
+            "search_string": None,
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 5,
+        },
+        {
+            "page_number": 0,
+            "page_size": 10,
+            "search_string": "Динаров Алмаз Антонович",  # покупатель
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 1,
+        },
+        {
+            "page_number": 0,
+            "page_size": 10,
+            "search_string": "Леонов",  # курьер
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 1,
+        },
+        {
+            "page_number": 1,
+            "page_size": 10,
+            "search_string": None,
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 5,
+        },
+        {
+            "page_number": 1,
+            "page_size": 3,
+            "search_string": None,
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 3,
+        },
+        {
+            "page_number": 2,
+            "page_size": 3,
+            "search_string": None,
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 2,
+        },
+        {
+            "page_number": 0,
+            "page_size": 10,
+            "search_string": "Айназ",  # такого пользователя не существует
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 0,
+        },
+        {
+            "page_number": 0,
+            "page_size": 10,
+            "search_string": "Анатольевна",  # поставщик
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 1,
+        },
+        {
+            "page_number": 0,
+            "page_size": 10,
+            "search_string": "Динар Олегович",  # менеджер
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 1,
+        },
+        {
+            "page_number": 0,
+            "page_size": 10,
+            "search_string": "Долотов Давид",  # администратор
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 1,
+        },
+        {
+            "page_number": 0,
+            "page_size": 10,
+            "search_string": "TestAdmin@gmail.com",  # администратор
+            "date_begin": None,
+            "date_end": None,
+            "role_uuid": None,
+            "count": 1,
+        },
+    ),
+)
