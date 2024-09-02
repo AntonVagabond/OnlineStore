48800460291f559be75900710170ce81b5ae90ea
Akuev <antonakuev945@mail.ru>
2024-09-02 16:01:53 +0300

    fix [Users]: Переделал Кастомный UOW.

--- src/apps/users/modules/unit_of_works/admin_panels.py
+++ src/apps/users/modules/unit_of_works/admin_panels.py
@@ -1,4 +1,5 @@
-from typing import Self
+from types import TracebackType
+from typing import Self, Optional
 from common.unit_of_works.base import BaseUnitOfWork
 from modules.repositories.admin_panels import AdminPanelRepository
 
@@ -8,7 +9,17 @@ class AdminPanelUOW(BaseUnitOfWork):
     repo = AdminPanelRepository
 
     async def __aenter__(self) -> Self:
-        """Вход в контекстного менеджера."""
+        """Вход в контекстного менеджера для панели администратора."""
         await super().__aenter__()
         self.repo = AdminPanelRepository(self._session)
         return self
+
+    async def __aexit__(
+            self,
+            exc_type: Optional[type[BaseException]],
+            exc_val: Optional[BaseException],
+            exc_tb: Optional[TracebackType],
+    ) -> None:
+        """Выход из контекстного менеджера для панели администратора."""
+        await super().__aexit__(exc_type, exc_val, exc_tb)
+        await self.close()
