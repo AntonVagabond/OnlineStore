c5be88c567c483ee240833c3e26795e017d4d8d8
Akuev <antonakuev945@mail.ru>
2024-09-03 15:58:51 +0300

    fix [Users]: Убрал устаревший параметр.

--- src/apps/users/common/schemas/base.py
+++ src/apps/users/common/schemas/base.py
@@ -1,15 +1,9 @@
 from typing import Any
 
-from pydantic import BaseModel as _BaseModel
+from pydantic import BaseModel as _BaseModel, ConfigDict
 
 
 class BaseModel(_BaseModel):
     """Базовая pydantic-модель."""
-    class Config:
-        @staticmethod
-        def json_schema_extra(schema: dict[str, Any], _: Any) -> None:
-            props = {}
-            for key, value in schema.get('properties', {}).items():
-                if not value.get("hidden", False):
-                    props[key] = value
-            schema["properties"] = props
+    model_config = ConfigDict()
+
