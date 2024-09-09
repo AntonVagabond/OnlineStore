80b9175b339ae54c4c6f6e832c24f8c98086e6fa
Akuev <antonakuev945@mail.ru>
2024-09-09 09:03:46 +0300

    fix [Users]: Перенес параметры pydantic-модели в профиле в общую модель.

--- src/apps/users/common/schemas/api/mixins.py
+++ src/apps/users/common/schemas/api/mixins.py
@@ -87,3 +87,40 @@ class RegisterSchema(BaseModel):
         """Преобразование даты рождения под нужный формат."""
         datetime_str = birthday.strftime("%Y-%m-%d %H:%M:%S")
         return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
+
+
+class UpdateSchema(BaseModel):
+    """Общая схема для редактирования."""
+    last_name: str
+    first_name: str
+    second_name: str
+    birthday: Optional[datetime] = Field(default=None)
+    phone_number: str
+    updated_at: HiddenField[datetime] = Field(default=datetime.now())
+
+    @field_validator("last_name")
+    def validate_last_name(cls, last_name: str) -> str:  # noqa
+        """Проверка отчества."""
+        if 2 <= len(last_name) <= 50:
+            return last_name
+        raise ValidationError("Символов в отчестве должно быть больше 1 либо меньше 51.")
+
+    @field_validator("first_name")
+    def validate_first_name(cls, first_name: str) -> str:  # noqa
+        """Проверка имени."""
+        if 2 <= len(first_name) <= 50:
+            return first_name
+        raise ValidationError("Символов в имени должно быть больше 1 либо меньше 51.")
+
+    @field_validator("second_name")
+    def validate_second_name(cls, second_name: str) -> str:  # noqa
+        """Проверка фамилии."""
+        if 2 <= len(second_name) <= 50:
+            return second_name
+        raise ValidationError("Символов в фамилии должно быть больше 1 либо меньше 51.")
+
+    @field_validator("birthday")
+    def validate_birthday(cls, birthday: datetime) -> datetime:  # noqa
+        """Преобразование даты рождения под нужный формат."""
+        datetime_str = birthday.strftime("%Y-%m-%d %H:%M:%S")
+        return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
--- src/apps/users/modules/schemas/profiles.py
+++ src/apps/users/modules/schemas/profiles.py
@@ -4,7 +4,7 @@ from uuid import UUID
 
 from pydantic import EmailStr, field_validator, ValidationError, Field
 
-from common.schemas.api.mixins import RegisterSchema
+from common.schemas.api.mixins import RegisterSchema, UpdateSchema
 from common.schemas.base import BaseModel
 
 
@@ -31,40 +31,5 @@ class RegisterUserSchema(RegisterSchema):
     """Схема для регистрации пользователя."""
 
 
-class UpdateUserSchema(BaseModel):
+class UpdateUserSchema(UpdateSchema):
     """Схема для обновления пользователя."""
-    last_name: str
-    first_name: str
-    second_name: str
-    birthday: Optional[datetime] = Field(default=None)
-    phone_number: str
-    updated_at: datetime = Field(
-        default=datetime.now(), json_schema_extra={"hidden": True},
-    )
-
-    @field_validator("last_name")
-    def validate_last_name(cls, last_name: str) -> str:  # noqa
-        """Проверка отчества."""
-        if 2 <= len(last_name) <= 50:
-            return last_name
-        raise ValidationError("Символов в отчестве должно быть больше 1 либо меньше 51.")
-
-    @field_validator("first_name")
-    def validate_first_name(cls, first_name: str) -> str:  # noqa
-        """Проверка имени."""
-        if 2 <= len(first_name) <= 50:
-            return first_name
-        raise ValidationError("Символов в имени должно быть больше 1 либо меньше 51.")
-
-    @field_validator("second_name")
-    def validate_second_name(cls, second_name: str) -> str:  # noqa
-        """Проверка фамилии."""
-        if 2 <= len(second_name) <= 50:
-            return second_name
-        raise ValidationError("Символов в фамилии должно быть больше 1 либо меньше 51.")
-
-    @field_validator("birthday")
-    def validate_birthday(cls, birthday: datetime) -> datetime:  # noqa
-        """Преобразование даты рождения под нужный формат."""
-        datetime_str = birthday.strftime("%Y-%m-%d %H:%M:%S")
-        return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
