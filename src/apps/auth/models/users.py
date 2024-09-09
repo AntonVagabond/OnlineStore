a830fd274d597651f1398368d3c4a61cfc6348bf
Akuev <antonakuev945@mail.ru>
2024-09-09 09:01:46 +0300

    fix [Auth]: Переделал класс пользователя

--- src/apps/auth/models/users.py
+++ src/apps/auth/models/users.py
@@ -4,80 +4,32 @@ from typing import Optional
 import sqlalchemy as sa
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from common.models.mixins import DateModelMixin, GenericMixin
+from common.models.base import Base
+from common.models.mixins import CreatedUpdatedMixin, UUIDMixin
 
 
-class User(DateModelMixin, GenericMixin[sa.UUID]):
+class User(Base, CreatedUpdatedMixin, UUIDMixin):
     """Модель пользователя."""
-    __tablename__ = "UserDB"
-
-    username: Mapped[Optional[str]] = mapped_column(
-        __name_pos="UserName",
-        __type_pos=sa.String(100),
-    )
-    last_name: Mapped[Optional[str]] = mapped_column(
-        __name_pos="LastName",
-        __type_pos=sa.String(100),
-    )
-    first_name: Mapped[Optional[str]] = mapped_column(
-        __name_pos="FirstName",
-        __type_pos=sa.String(100),
-    )
-    second_name: Mapped[Optional[str]] = mapped_column(
-        __name_pos="SecondName",
-        __type_pos=sa.String(100),
-    )
-    full_name: Mapped[Optional[str]] = mapped_column(
-        __name_pos="FullName",
-        __type_pos=sa.String(200),
-    )
+    username: Mapped[Optional[str]] = mapped_column(sa.String(length=100))
+    last_name: Mapped[Optional[str]] = mapped_column(sa.String(length=100))
+    first_name: Mapped[Optional[str]] = mapped_column(sa.String(length=100))
+    second_name: Mapped[Optional[str]] = mapped_column(sa.String(length=100))
+    full_name: Mapped[Optional[str]] = mapped_column(sa.String(length=200))
     email: Mapped[Optional[str]] = mapped_column(
-        __name_pos="Email",
-        __type_pos=sa.String(256),
-        unique=True,
+        sa.String(length=256), index=True, unique=True,
     )
-    password_hash: Mapped[Optional[bytes]] = mapped_column(
-        __name_pos="PasswordHash",
-        __type_pos=sa.LargeBinary,
-    )
-    phone_number: Mapped[Optional[str]] = mapped_column(
-        __name_pos="PhoneNumber",
-        __type_pos=sa.String(18),
-    )
-    photo: Mapped[Optional[str]] = mapped_column(
-        __name_pos="Photo",
-        __type_pos=sa.String(100),
-    )
-    birthday: Mapped[Optional[datetime]] = mapped_column(
-        __name_pos="Birthday",
-        __type_pos=sa.DateTime,
-    )
+    password_hash: Mapped[Optional[bytes]] = mapped_column(sa.LargeBinary)
+    phone_number: Mapped[Optional[str]] = mapped_column(sa.String(length=18))
+    photo: Mapped[Optional[str]] = mapped_column(sa.String(length=100))
+    birthday: Mapped[Optional[datetime]] = mapped_column(sa.DateTime)
     role: Mapped["Role"] = relationship(  # type: ignore
-        back_populates="users",
-        lazy="raise",
-        default=None,
+        back_populates="users", lazy="raise",
     )
-    logged_out: Mapped[bool] = mapped_column(
-        __name_pos="LoggedOut",
-        __type_pos=sa.Boolean,
-        default=False,
-        nullable=False
-    )
-    deleted: Mapped[bool] = mapped_column(
-        __name_pos="Deleted",
-        __type_pos=sa.Boolean,
-        default=False,
-        nullable=False
-    )
-    is_man: Mapped[bool] = mapped_column(
-        __name_pos="IsMan",
-        __type_pos=sa.Boolean,
-        default=False,
-        nullable=False,
-    )
+    logged_out: Mapped[bool] = mapped_column(sa.Boolean, default=False, nullable=False)
+    deleted: Mapped[bool] = mapped_column(sa.Boolean, default=False, nullable=False)
+    is_man: Mapped[bool] = mapped_column(sa.Boolean, default=False, nullable=False)
     role_id: Mapped[Optional[sa.UUID]] = mapped_column(
-        "RoleId",
         sa.UUID,
-        sa.ForeignKey(column="RoleDB.Id", ondelete="RESTRICT"),
+        sa.ForeignKey(column="role.id", ondelete="RESTRICT"),
         default=None,
     )
