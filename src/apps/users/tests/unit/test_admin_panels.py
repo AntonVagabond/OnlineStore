f7653e58b6aa8870d15abcab2305cea11cb3f9bc
Akuev <antonakuev945@mail.ru>
2024-09-11 10:16:41 +0300

    feat [Users]: Добавил тест получения пользователя для редактирования Администратором.

--- src/apps/users/tests/unit/test_admin_panels.py
+++ src/apps/users/tests/unit/test_admin_panels.py
@@ -1,17 +1,17 @@
 from typing import Optional, Union
+from uuid import UUID
 
 import pytest
 
+from common.const.response_exceptions import USER_NOT_FOUND
 from common.exceptions import mixins as exceptions
 from modules.schemas.admin_panels import UserByRoleFilterSchema, UpdateAdminSchema
 from modules.services.admin_panels import AdminPanelService
-from tests.unit.data.admin_panels import (
-    GET_LIST_USERS_ADMIN_TEST_DATA, UPDATE_USER_ADMIN_TEST_DATA,
-)
+from tests.unit.data import admin_panels as data_array
 from tests.unit.fixtures.admin_panels import AdminTestUOW
 
 
-@pytest.mark.parametrize(*GET_LIST_USERS_ADMIN_TEST_DATA)
+@pytest.mark.parametrize(*data_array.GET_LIST_USERS_ADMIN_TEST_DATA)
 async def test_get_list_users_admin(
         admin_panel_uow: AdminTestUOW,
         admin_panel_service: AdminPanelService,
@@ -25,7 +25,24 @@ async def test_get_list_users_admin(
     assert len(response.records) == count
 
 
-@pytest.mark.parametrize(*UPDATE_USER_ADMIN_TEST_DATA)
+@pytest.mark.parametrize(*data_array.GET_USER_TEST_DATA)
+async def test_get_user(
+        admin_panel_uow: AdminTestUOW,
+        admin_panel_service: AdminPanelService,
+        uuid: UUID,
+        test_result: bool,
+) -> None:
+    """Тест для получения данных пользователя для редактирования."""
+    if test_result == USER_NOT_FOUND:
+        with pytest.raises(exceptions.UserNotFoundException) as exc:
+            await admin_panel_service.get(admin_panel_uow, uuid)
+        assert exc.value.detail == test_result
+    else:
+        user = await admin_panel_service.get(admin_panel_uow, uuid)
+        assert bool(user) == test_result
+
+
+@pytest.mark.parametrize(*data_array.UPDATE_USER_ADMIN_TEST_DATA)
 async def test_update_user_admin(
         admin_panel_uow: AdminTestUOW,
         admin_panel_service: AdminPanelService,
